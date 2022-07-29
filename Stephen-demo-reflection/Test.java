bfb0c54bf67bc5d1c9ceaf2d4db7fa17fe2add3b
Scifiler-sys <pagdilaostephen@gmail.com>
2022-07-29 15:25:31 -0400

    Added one more annotation

--- Stephen-demo-reflection/Test.java
+++ Stephen-demo-reflection/Test.java
@@ -6,6 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 //This is an annotation to tell our custom annotation to keep this annotation during runtime
 //TDLR: we can use it in our reflection
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD) //We can also add this annotation to indicate that our Test annotation can only be applied to methods
 public @interface Test {
     
     //Will determine if it will run the unit test or not
